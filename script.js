977f526219aee420fc132793669425d1998e9bf9
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2024-08-31 03:45:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let selectedGame = null;
 
     sourceCode.addEventListener('click', () => {
-        window.open('https://github.com/ShafiqSadat/HamsterKeyGenWeb', '_blank');
+        window.open('https://t.me/hiddengemnews', '_blank');
     });
     
 gameOptions.forEach(option => {
